d239cc31862fff4054aa824655d7323d624a8b12
Coen ten Thije Boonkkamp <coen@coenttb.com>
2025-12-03 12:57:39 +0100

    Add Flow enum and isVoid to WHATWG_HTML.Element protocol
    - Add Flow enum (.block, .inline) with init for typed elements
    - Add isVoid static property requirement with default implementation
    - Both use switch-based logic as single source of truth
    - Enables type-safe HTML rendering with proper formatting

--- Sources/WHATWG HTML Shared/Element.swift
+++ Sources/WHATWG HTML Shared/Element.swift
@@ -31,18 +31,87 @@ extension WHATWG_HTML {
         ///
         /// For example, `"div"`, `"form"`, `"input"`, etc.
         static var tag: String { get }
+        static var flow: WHATWG_HTML.Flow { get }
+        static var isVoid: Bool { get }
     }
 }
 
 extension WHATWG_HTML.Element {
-    /// Whether this element is a void element (self-closing)
-    ///
-    /// Void elements cannot have content and don't have closing tags.
-    /// Examples: `<br>`, `<img>`, `<input>`, `<meta>`, etc.
     @inlinable public static var isVoid: Bool {
-        Self.self is any WHATWG_HTML.VoidElement.Type
+        WHATWG_HTML.isVoid(for: Self.tag)
     }
 }
+
+extension WHATWG_HTML.Element {
+    @inlinable public static var flow: WHATWG_HTML.Flow {
+        .init(Self.self)
+    }
+}
+
+extension WHATWG_HTML {
+    public enum Flow: String, Sendable, Hashable, Codable {
+        case block
+        case inline
+    }
+}
+
+//extension Void {
+//    public init<Element: WHATWG_HTML.Element>(
+//        _ element: Element.Type
+//    ){
+//        switch Self.tag {
+//        case "area", "base", "br", "col", "embed", "hr", "img", "input",
+//            "link", "meta", "source", "track", "wbr":
+//            return true
+//        default:
+//            return false
+//        }
+//    }
+//}
+
+extension WHATWG_HTML.Flow {
+    /// Creates a Flow value for a typed element.
+    public init<Element: WHATWG_HTML.Element>(
+        _ element: Element.Type
+    ) {
+        self.init(for: Element.tag)
+    }
+
+    /// Creates a Flow value for a tag name string.
+    /// Single source of truth for flow classification.
+    public init(for tag: String) {
+        switch tag {
+        case "a", "abbr", "b", "bdi", "bdo", "br", "cite", "code", "data",
+            "dfn", "em", "i", "kbd", "mark", "q", "rp", "rt", "ruby",
+            "s", "samp", "small", "span", "strong", "sub", "sup", "time",
+            "u", "var", "wbr",
+            "button", "input", "label", "select", "textarea",
+            "audio", "canvas", "embed", "iframe", "img", "object",
+            "picture", "video",
+            "script", "noscript", "slot", "template":
+            self = .inline
+        default:
+            self = .block
+        }
+    }
+}
+
+extension WHATWG_HTML {
+    /// Checks if a tag name represents a void element.
+    /// Single source of truth for void element classification.
+    @inlinable
+    public static func isVoid(for tag: String) -> Bool {
+        switch tag {
+        case "area", "base", "br", "col", "embed", "hr", "img", "input",
+            "link", "meta", "source", "track", "wbr":
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+
 
 extension WHATWG_HTML {
     /// Marker protocol for void (self-closing) HTML elements
